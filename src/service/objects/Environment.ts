6846e84a3662d28efaf42e750ddca215576d6322
mj khodadadi <m_javad_96@yahoo.com>
2022-07-21 01:47:13 -0700

    7/21 0146am {updating blender scene and aqu light}

--- src/service/objects/Environment.ts
+++ src/service/objects/Environment.ts
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
+import { RectAreaLightUniformsLib } from "three/examples/jsm/lights/RectAreaLightUniformsLib";
 import Service from "../Service";
 
 export default class Environment {
@@ -10,24 +12,25 @@ export default class Environment {
   lamp1?: THREE.PointLight;
   lamp2?: THREE.PointLight;
   lamp3?: THREE.PointLight;
-  aquLamp?: THREE.Light;
+  aquLamp?: THREE.RectAreaLight;
 
   constructor() {
     this.service = new Service();
     this.scene = this.service.scene;
 
     // this.createSunlight();
-    // this.createAmbientlight();
+    this.createAmbientlight();
     this.createPointLight();
     this.addHelpers();
     // this.addGui();
+    this.createRectAreaLight();
   }
 
   createPointLight() {
     this.lamp1 = new THREE.PointLight(0xffaa00, 1);
     // this.lamp1.position.set(0, 0, 0);
     this.scene?.add(this.lamp1);
-    this.lamp1.position.set(1.75, 0.6, -1.16);
+    this.lamp1.position.set(2.99, 0.57, 0.43);
     this.lamp1.castShadow = true;
     // this.lamp1.shadow.camera.far = 20;
     // this.lamp1.shadow.mapSize.set(1024, 1024);
@@ -35,7 +38,7 @@ export default class Environment {
 
     this.lamp2 = new THREE.PointLight(0xffaa00, 1);
     this.scene?.add(this.lamp2);
-    this.lamp2.position.set(-0.16, 0.51, -1.77);
+    this.lamp2.position.set(1.16, 0.57, -0.13);
     this.lamp2.castShadow = true;
     // this.lamp2.shadow.camera.far = 20;
     // this.lamp2.shadow.mapSize.set(1024, 1024);
@@ -43,7 +46,7 @@ export default class Environment {
 
     this.lamp3 = new THREE.PointLight(0xffaa00, 1);
     this.scene?.add(this.lamp3);
-    this.lamp3.position.set(1.19, 1.58, -1.59);
+    this.lamp3.position.set(2.58, 1.6, -0.15);
     this.lamp3.castShadow = true;
     // this.lamp3.shadow.camera.far = 20;
     // this.lamp3.shadow.mapSize.set(1024, 1024);
@@ -53,6 +56,19 @@ export default class Environment {
     // lamp1?.add(this.lamp1!.position, "z", -10, 10);
     // const lamp2 = this.Gui?.Gui.addFolder("lamp2");
     // lamp2?.add(this.lamp2!.position, "z", -10, 10);
+  }
+
+  createRectAreaLight() {
+    RectAreaLightUniformsLib.init();
+    const width = 0.85;
+    const height = 0.47;
+    this.aquLamp = new THREE.RectAreaLight(0xffffff, 6, width, height);
+    this.aquLamp.position.set(0, 0.71, 0);
+    this.aquLamp.lookAt(0, 2, 0);
+    // this.aquLamp.lookAt(-1, 25, 10);
+    this.scene?.add(this.aquLamp);
+    const aquLampHelper = new RectAreaLightHelper(this.aquLamp!);
+    this.scene?.add(aquLampHelper);
   }
 
   createSunlight() {
@@ -67,7 +83,7 @@ export default class Environment {
   }
 
   createAmbientlight() {
-    this.ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
     this.scene?.add(this.ambientLight);
   }
 
@@ -86,8 +102,8 @@ export default class Environment {
     // const sunlightHelper = new THREE.DirectionalLightHelper(this.sunLight!);
     // this.scene?.add(sunlightHelper);
 
-    const lamp1Helper = new THREE.PointLightHelper(this.lamp1!, 0.1);
-    this.scene?.add(lamp1Helper);
+    // const lamp1Helper = new THREE.PointLightHelper(this.lamp1!, 0.1);
+    // this.scene?.add(lamp1Helper);
   }
 
   addGui() {
